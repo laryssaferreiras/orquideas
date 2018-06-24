85d815fae515d0129660b7a586ea3fa056cb720f
Laryssa Ferreira <lary.13.bsb@hotmail.com>
2018-06-24 15:59:31 -0300

    Add index.php

---
+++ html/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Orquídeas</title>
+		<link rel="stylesheet" href="../css/bootstrap.min.css">
+	</head>
+	<body>
+		<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">  
+			<ul class="navbar-nav">
+				<li class="nav-item">
+					<a href="index.php" class="nav-link">
+						Home
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</body>
+<html>
